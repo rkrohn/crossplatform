c2bfee368e3cda54c854b93933f094395c5ce84d
Rachel Krohn <rkrohn@nd.edu>
2019-09-18 13:05:04 -0400

    doing things - keyword searching and frequency analysis

--- load_data.py
+++ load_data.py
@@ -22,6 +22,9 @@ twitter_data_files = {
 					"botometer_ar": "/data/socsim/2019DecCP/White_Helmet/Twitter/Tng_an_ar_Twitter_WH_botometer_results.json.gz"
 }
 
+#file containing mapping from search term -> narrative label component
+search_term_mapping_file = "./data/search_term_mapping.csv"
+
 
 #given a dictionary of data type -> filename, load domain data
 def load_domain_data(type_to_files):
@@ -99,6 +102,7 @@ def narrative_analysis(data, datatype=""):
 	return {"unlabeled_count": no_label, "label_freq": label_counts, "comp_freq": comp_counts}
 #end narrative_analysis
 
+
 #perform narrative label analysis on all data for an entire platform (across all data types)
 def platform_narrative_analysis(data, platform=""):
 	narr_res = {}		#counts for each datatype
@@ -117,25 +121,212 @@ def platform_narrative_analysis(data, platform=""):
 	return narr_res, uniq_labels, uniq_comps
 #end platform_narrative_analysis
 
+
+#search data for narrative keywords
+#only search the list of fields given as argument (for flexibility)
+#each field will itself be a list, where subsequent entries are nested below the previous
+#skip any fields that don't exist (because some are optional, ugh)
+def search_narrative_keywords(data, fields, keywords_dict):
+	#store item narratives in nested dict
+	#id_h -> given-> given narrative label
+	#id_h -> inferred -> inferred narrative label, based on keywords
+	item_narratives = {}	 
+
+	#loop each object
+	for item in data:
+		#pull given narrative label
+		item_narratives[item['id_h']] = {'inferred': []}
+		item_narratives[item['id_h']]['given'] = item['extension']['socialsim_information_id'] if item['extension']['socialsim_information_id'][0] != '' else []
+
+		#search each field
+		for field in fields:
+			#grab desired text field
+			text = item
+			for subfield in field:
+				if subfield in text:
+					text = text[subfield]
+				else:
+					text = None
+					break
+
+			#skip if bad text (ie, skipping field)
+			if text is None: break
+
+			#if text is a list (tags), make into a string
+			if isinstance(text, list):
+				text = ' '.join(text)
+
+			#convert text to all lowercase
+			text = text.lower()
+
+			#print(field[-1], "text:", text)
+
+			#look for all keywords in this text
+			for keyword, label in keywords_dict.items():
+				if keyword in text and label not in item_narratives[item['id_h']]['inferred']:
+					item_narratives[item['id_h']]['inferred'].append(label)
+					#print("\0u' %s ' in %s -> %s" % (keyword, field[-1], label))
+
+		#print("given", item_narratives[item['id_h']]['given'])
+		#print("inferred", item_narratives[item['id_h']]['inferred'])
+
+	return item_narratives		#return dictionary
+#end search_narrative_keywords
+
+
+#given assigned narrative labels, break into set of components present in any narrative label
+def narratives_to_comp(all_narrative_dict):
+	#loop all objects
+	for obj_id, narrative_dict in all_narrative_dict.items():
+		#new dict entry for list of given components
+		narrative_dict['given_comp'] = []
+		#loop each narrative label
+		for label in narrative_dict['given']:
+			#and split
+			for comp in label.split('-'):
+				if comp not in narrative_dict['given_comp']:
+					narrative_dict['given_comp'].append(comp)
+	#return updated dict
+	return all_narrative_dict
+#end narratives_to_comp
+
+
+#write narrative frequencies to file
+def save_narrative_freq(narr_dict, labels, components, filename):
+	#first column will be list of labels/components, following will be frequencies for each datatype
+	#two separate files for labels and components (argument is basefilename)
+
+	#build lists for labels file
+	col_headers = ['narrative_label']
+	all_cols = [list(labels)]		#start list of all columns, where first col is list of narr labels
+	#new column list for each data type
+	for datatype, freq_dict in narr_dict.items():
+		new_col = []
+		for label in all_cols[0]:
+			new_col.append(freq_dict['label_freq'][label] if label in freq_dict['label_freq'] else 0)
+		col_headers.append(datatype+"_freq")
+		all_cols.append(new_col)
+	#save labels
+	file_utils.lists_to_csv(all_cols, col_headers, filename+"_narrative_labels.csv")
+
+	#and the same for narrative components
+	col_headers = ['narrative_components']
+	all_cols = [list(components)]		#start list of all columns, where first col is list of narr labels
+	#new column list for each data type
+	for datatype, freq_dict in narr_dict.items():
+		new_col = []
+		for label in all_cols[0]:
+			new_col.append(freq_dict['comp_freq'][label] if label in freq_dict['comp_freq'] else 0)
+		col_headers.append(datatype+"_freq")
+		all_cols.append(new_col)
+	#save labels
+	file_utils.lists_to_csv(all_cols, col_headers, filename+"_narrative_components.csv")
+
+	print("\nFrequency data saved to %s_narrative_labels.csv and %s_narrative_components.csv" % (filename, filename))
+#end save_narrative_freq
+
+
+
+
 #----- MAIN EXECUTION -----#
 
+print("")
+
+#load search term -> narrative component mapping
+search_term_dict = file_utils.read_csv_dict(search_term_mapping_file)
+
+print(len(search_term_dict), "search keywords")
+#print(list(search_term_dict.keys()))
+
+
 #load the youtube data
-print("Loading YouTube data")
+print("\nLoading YouTube data")
 youtube_data = load_domain_data(youtube_data_files)
-
-#narrative analysis
-youtube_narr_res, youtube_labels, youtube_comps = platform_narrative_analysis(youtube_data, "YouTube")
-print(sorted(youtube_comps))
-print(sorted(youtube_labels))
-
-#exit(0)
 
 #and the Twitter
 print("\nLoading Twitter data")
 twitter_data = load_domain_data(twitter_data_files)
 
 #narrative analysis
+#YouTube
+youtube_narr_res, youtube_labels, youtube_comps = platform_narrative_analysis(youtube_data, "YouTube")
+#Twitter
+#narrative analysis
 twitter_narr_res, twitter_labels, twitter_comps = platform_narrative_analysis(twitter_data, "Twitter")
-print(sorted(twitter_comps))
-print(sorted(twitter_labels))
+
+#combine into single list of labels and components
+all_labels = youtube_labels.union(twitter_labels)
+all_comps = youtube_comps.union(twitter_comps)
+print("\n%d labels, %d components across both platforms" % (len(all_labels), len(all_comps)))
+
+#save frequencies to files (4 separate for now, can't be bothered to combine further)
+save_narrative_freq(youtube_narr_res, all_labels, all_comps, "results/youtube_freq")
+save_narrative_freq(twitter_narr_res, all_labels, all_comps, "results/twitter_freq")
+
+
+
+exit(0)
+
+#does the video title/description/tags lead to the same narrative labels?
+#videos_narrative_dict = search_narrative_keywords(youtube_data['videos'], [["snippet","title_m"], ["snippet","description_m"], ["snippet","tags"]], search_term_dict)
+'''
+print("\nvideos with given narrative, vs inferred")
+for video_id, narrative_dict in videos_narrative_dict.items():
+	if len(narrative_dict['given']) > 0:
+		print(video_id, narrative_dict['given'], narrative_dict['inferred'])
+'''
+
+#convert given/assigned narrative labels to list of components - for easier comparison against inferred
+#videos_narrative_dict = narratives_to_comp(videos_narrative_dict)
+'''
+print("\nvideos with given narrative components, vs inferred")
+for video_id, narrative_dict in videos_narrative_dict.items():
+	if len(narrative_dict['given_comp']) > 0:
+		print(video_id, narrative_dict['given_comp'], narrative_dict['inferred'])
+'''
+
+'''
+#which given components did we not find?
+print("\nvideos and narrative components not found via keywords")
+missing_comp_count = 0	#count of videos with missing components
+for video_id, narrative_dict in videos_narrative_dict.items():
+	vid_count = 0
+	for comp in narrative_dict['given_comp']:
+		if comp not in narrative_dict['inferred']:
+			if vid_count == 0: print(video_id, ":", end=" ")
+			print(comp, end=" ")
+			vid_count += 1
+	if vid_count != 0: 
+		print("")
+		missing_comp_count += 1
+print(missing_comp_count, "videos missing given components (of", len(youtube_data['videos']) - youtube_narr_res['videos']['unlabeled_count'], "labelled)")
+
+#single video - how are we missing keywords?
+
+print("")
+vid_id = "0FPAb4yCjI1Jfyzy_YyYiA"
+for video in youtube_data['videos']:
+	if video['id_h'] == vid_id:
+		print("title:", video['snippet']['title_m'])
+		print("description:", video['snippet']['description_m'])
+print("")
+print(videos_narrative_dict[vid_id])
+
+exit(0)
+
+#are there duplicate videos in the dataset?
+print("")
+print(len(youtube_data['videos']), "videos in list")
+print(len(set(video['id_h'] for video in youtube_data['videos'])), "videos in set")
+
+#who are the duplicates, and are they the same?
+#list of ids in list
+id_list = [video['id_h'] for video in youtube_data['videos']]
+#duplicate ids
+id_duplicates = set([x for x in id_list if id_list.count(x) > 1])
+print(len(id_duplicates), "duplicates:", id_duplicates)
+
+exit(0)
+'''
+
 
